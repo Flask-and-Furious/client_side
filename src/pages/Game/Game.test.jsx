258dc8c60c8ce97c49f4b20a99bff725f9b9510d
vivian <vivian.v18723@gmail.com>
2022-09-23 21:28:56 +0100

    add passing tests

--- src/pages/Game/Game.test.jsx
+++ src/pages/Game/Game.test.jsx
@@ -2,12 +2,13 @@
  * @jest-environment jsdom
  */
 
-import { screen, render } from "@testing-library/react";
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
+import { screen, render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 
-import Game from "../index";
+import Game from ".";
 
 describe("Running Game component", () => {
   test("Ensures it renders without crashing", () => {
@@ -19,7 +20,7 @@ describe("Running Game component", () => {
   });
 });
 
-describe("Game Component Content", () => {
+describe("Submitting Game Component Content", () => {
   beforeEach(() => {
     render(
       <BrowserRouter>
@@ -28,9 +29,19 @@ describe("Game Component Content", () => {
     );
   });
 
-  test("Ensures title is present for user to read", () => {
-    const title = screen.getByRole("heading");
-    expect(title).toBeInTheDocument();
-    expect(title).toBeTruthy();
+  test("Ensures input is cleared after submission (hitting enter)", () => {
+    const answerInput = screen.getByRole("input");
+
+    userEvent.type(answerInput, "test answer{enter}");
+    expect(answerInput.value).toBe("");
+  });
+
+  test("Ensures input is cleared after submission (clicking submit button)", () => {
+    const clickSubmit = screen.getByRole("button");
+    const answerInput = screen.getByRole("input");
+
+    userEvent.type(answerInput, "test answer");
+    userEvent.click(clickSubmit);
+    expect(answerInput.value).toBe("");
   });
 });
