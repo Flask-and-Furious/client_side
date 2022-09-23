4fde8efed6bc82131bce6a9eb190dac8f52fc09b
johnpascual <ashleypascual@hotmail.es>
2022-09-23 14:57:41 +0100

    fix login and register pages

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
 export { default as Option } from "./Option";
 export { default as Subtitle } from "./Subtitle";
 export { default as Title } from "./Title";
+export { default as Input } from "./Input";
