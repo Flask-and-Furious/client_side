fac5b6f13b22ec928def3721baf142731d003c14
vivian <vivian.v18723@gmail.com>
2022-09-23 15:20:35 +0100

    create game component

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Button, Image, Input, Subtitle, Title } from "../../components";
 
-function HomePage() {
-    return (
-        <div>Hello World</div>
-    )
+function Game() {
+  return (
+    <>
+      <Title title="Debugging Challenge" />
+      <Subtitle subtitle="Question 1" />
+      <Image image="" />
+      <Input name="" classVariant="" text="Which line number is wrong?" />
+      <Button text="Submit" />
+    </>
+  );
 }
 
-export default HomePage
+export default Game;
