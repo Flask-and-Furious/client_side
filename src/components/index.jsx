f82be47f3ef97903ce8a0752465a14c9fbf04ad1
vivian <vivian.v18723@gmail.com>
2022-09-23 15:41:56 +0100

    export btn, image & input

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,9 @@
+export { default as Button } from "./Button";
+export { default as Image } from "./Image";
+export { default as Input } from "./Input";
 export { default as Option } from "./Option";
 export { default as Subtitle } from "./Subtitle";
 export { default as Title } from "./Title";
+
+
+
