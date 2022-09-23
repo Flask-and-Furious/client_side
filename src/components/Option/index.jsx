20b14a5ed861d95a8b560eb822c12e7e4fe6c54f
vivian <vivian.v18723@gmail.com>
2022-09-23 23:26:48 +0100

    edit htmlFor attribute

--- src/components/Option/index.jsx
+++ src/components/Option/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Option({ topic, forAndIdAttr, nameAttr }) {
   return (
     <>
-      <label for={forAndIdAttr} role="label">{topic}</label>
+      <label htmlFor={forAndIdAttr} role="label">{topic}</label>
       <input type="radio" name={nameAttr} id={forAndIdAttr}  role="input"/>
     </>
   );
