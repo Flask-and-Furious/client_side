238c64580e77bb43cd8502cf8b74f2efd6526244
vivian <vivian.v18723@gmail.com>
2022-09-24 00:53:11 +0100

    export PageNotFound component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,9 @@
-export { default as Home } from "./Home"
-export { default as LoginPage } from "./Login"
-export { default as RegisterPage } from "./Register"
-export { default as LanguagePage } from "./Language"
-export { default as DifficultyPage } from "./Difficulty"
-export { default as GamePage } from "./Game"
-export { default as CompletedPage } from "./Completed"
-export { default as DashboardPage } from "./Dashboard"
+export { default as Home } from "./Home";
+export { default as LoginPage } from "./Login";
+export { default as RegisterPage } from "./Register";
+export { default as LanguagePage } from "./Language";
+export { default as DifficultyPage } from "./Difficulty";
+export { default as GamePage } from "./Game";
+export { default as CompletedPage } from "./Completed";
+export { default as DashboardPage } from "./Dashboard";
+export { default as PageNotFound } from "./PageNotFound";
