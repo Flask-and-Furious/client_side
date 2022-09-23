2126958b72ef85bc3b0640e049a1e3dcdf810381
vivian <vivian.v18723@gmail.com>
2022-09-23 13:27:45 +0100

    change paths

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,3 @@
 export { default as Option } from "./Option";
-export { default as Article1 } from "./Subtitle/Article1";
-export { default as Article2 } from "./Subtitle/Article2";
-export { default as CatchySlogan } from "./Subtitle/CatchySlogan";
-export { default as ChooseLanguage } from "./Subtitle/ChooseLanguage";
-export { default as Dashboard } from "./Subtitle/Dashboard";
-export { default as QuestionNum } from "./Subtitle/QuestionNum";
+export { default as Subtitle } from "./Subtitle";
 export { default as Title } from "./Title";
