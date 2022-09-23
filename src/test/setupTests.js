b460a90dd5f90fb3ab1de4ce36b73cb5a0110b10
vivian <vivian.v18723@gmail.com>
2022-09-23 19:50:26 +0100

    remove unused file

--- src/test/setupTests.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import "@testing-library/jest-dom/extend-expect";
-
-global.React = React;
-global.render = render;
-global.userEvent = userEvent;
