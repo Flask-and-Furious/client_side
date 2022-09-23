0778553d575cd92d4a4b37a724aced31cd65cbbf
vivian <vivian.v18723@gmail.com>
2022-09-23 13:20:34 +0100

    export title, subtitle & option components

---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+export { default as Option } from "./Option";
+export { default as Article1 } from "./Subtitle/Article1";
+export { default as Article2 } from "./Subtitle/Article2";
+export { default as CatchySlogan } from "./Subtitle/CatchySlogan";
+export { default as ChooseLanguage } from "./Subtitle/ChooseLanguage";
+export { default as Dashboard } from "./Subtitle/Dashboard";
+export { default as QuestionNum } from "./Subtitle/QuestionNum";
+export { default as Title } from "./Title";
