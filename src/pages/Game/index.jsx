78500b0910c321719ec6929a2f88df72163c42cc
azotamiota <azotamiota@gmail.com>
2022-09-23 20:27:16 +0100

    first steps with code mirror

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState }from "react";
+import CodeMirror from '@uiw/react-codemirror'
+import axios from 'axios'
+// import 'codemirror/keymap/sublime';
+// import 'codemirror/theme/dracula.css';
 
 import { Button, Image, Input, Subtitle, Title } from "../../components";
 
+
 function Game() {
+  
+  const [code, setCode] = useState('const a = 0')
+  const submitCode = () => {
+    axios.post('http;//localhost:3000/code')
+    console.log(code)
+  }
+
   return (
     <>
       <Title title="Debugging Challenge" />
-      <Subtitle subtitle="Question 1" />
-      <Image image="" />
+      <Subtitle subtitle="Expected output: hello(name) // Hello, Jack!" />
+      <CodeMirror value={code} 
+              options={{
+                // theme: 'monokai',
+                // keyMap: 'sublime',
+                mode: 'jsx',
+              }}
+              onChange={(editor, change) => {
+                setCode(editor)
+              }} />
+      {/* <Image image="" /> */}
       <Input name="" text="Which line number is wrong?" />
-      <Button text="Submit" />
+      <div onClick={submitCode}><Button text="Submit" /></div>
     </>
   );
 }
