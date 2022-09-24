bdb28447592addb7a815e2ad9f50c634ec9408ac
azotamiota <azotamiota@gmail.com>
2022-09-24 22:58:07 +0100

    add hidden stopwatch to Game component

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }from "react";
+import React, { useEffect, useRef, useState }from "react";
 import { useNavigate } from "react-router-dom";
 
 import axios from 'axios'
@@ -16,18 +16,21 @@ function Game() {
   
   // Here fetch the information about the logged in user's progress. This will be a number. 
   // For example if he/she solved the first bug earlier, we change the "progress" to 2 with "setProgress(2)"
-  
   const navigate = useNavigate()
   const [currentCodePackage, setCurrentCodePackage] = useState(fetchedCodePackages[progress]) 
   const [isCorrect, setIsCorrect] = useState(false)
   const [isAnswered, setIsAnswered] = useState(false)
-  const [buttonState, setButtonState] = useState(false)
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false)
+  const [solvingTime, setSolvingTime] = useState(new Date().getTime())
   const [randomIndex, setRandomIndex] = useState(null) // This is only for random feedback messages, not important
 
   useEffect(() => { // update and reset information when jumping to the next question
     setCurrentCodePackage(fetchedCodePackages[progress])
     setIsAnswered(false)
     setIsCorrect(false)
+    setIsButtonDisabled(false)
+    setSolvingTime(new Date().getTime())
+
   }, [progress])
 
   const submitCode = async () => {
@@ -38,7 +41,11 @@ function Game() {
         if (data.data == currentCodePackage['snippet']['return']) { // we compare the incoming value with the saved return value from the database
           setIsCorrect(true)            // if it matches
           setRandomIndex(() => Math.floor(Math.random() * correctMessages.length))
-          setButtonState(true)
+          setIsButtonDisabled(true)
+          const doneTime = new Date().getTime()
+          const solvingSeconds = Math.round((doneTime - solvingTime) / 100) / 10
+          setSolvingTime(solvingSeconds)
+          // Here some code to save user's time to solve this bug. Maybe update if this is the quickest?
         } else {
           setIsCorrect(false)           // if it doesn't
           setRandomIndex(() => Math.floor(Math.random() * incorrectMessages.length))
@@ -49,6 +56,7 @@ function Game() {
   }
 
   const nextCode = () => {
+
     setProgress(prev => prev + 1)
     if (progress + 1 === fetchedCodePackages.length) {
       navigate('/completed')
@@ -63,6 +71,7 @@ function Game() {
       <FlashMessage style={{display: isAnswered ? 'flex' : 'none'}} text={isCorrect ? `✅${correctMessages[randomIndex]}` :
         `❌${incorrectMessages[randomIndex]}`}/> 
       <div style={{textAlign: 'start', margin: '20px', fontSize: '18px'}}>
+        <FlashMessage style={{display: isCorrect ? 'flex' : 'none'}} text={`${solvingTime} s`}/>
       <CodeMirror value={currentCodePackage['snippet']['body']} 
               theme={dracula}
               onChange={(editor, change) => {
@@ -82,7 +91,7 @@ function Game() {
       </div>
       {/* <Image image="" /> */}
       {/* <Input name="" text="Which line number is wrong?" /> */}
-      <div onClick={submitCode}><Button text="Submit" isDisabled={buttonState} /></div>
+      <div onClick={submitCode}><Button text="Submit" isDisabled={isButtonDisabled} /></div>
       <div onClick={nextCode} style={{display: isCorrect ? 'block' : 'none'}}><Button text="Next" /></div>
     </>
   );
