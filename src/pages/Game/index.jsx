9215530e5cbeb8de447226009aedc7cb93e4f0ac
azotamiota <azotamiota@gmail.com>
2022-09-25 23:36:20 +0100

    add javascript language option

--- src/pages/Game/index.jsx
+++ src/pages/Game/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState }from "react";
+import React, { useEffect, useState }from "react";
 import { useNavigate } from "react-router-dom";
 
 import axios from 'axios'
@@ -6,18 +6,23 @@ import CodeMirror from '@uiw/react-codemirror'
 import { dracula } from '@uiw/codemirror-theme-dracula'; // code window theme
 import { langs } from '@uiw/codemirror-extensions-langs'; // font themes for different languages
 
-import { fetchedCodePackages } from "./questions";
+import { pythonFetchedCodePackages } from "./pythonQuestions";
+import { javascriptFetchedCodePackages } from "./javascriptQuestions";
 import { correctMessages, incorrectMessages } from "./feedback";
 import { Button, Image, Input, Subtitle, Title, FlashMessage } from "../../components";
 
 function Game() {
 
+  // const language = 'python' // language will be passed by props
+  const language = 'javascript' // language will be passed by props
+
   const [progress, setProgress] = useState(0)
   
   // Here fetch the information about the logged in user's progress. This will be a number. 
   // For example if he/she solved the first bug earlier, we change the "progress" to 2 with "setProgress(2)"
+
   const navigate = useNavigate()
-  const [currentCodePackage, setCurrentCodePackage] = useState(fetchedCodePackages[progress]) 
+  const [currentCodePackage, setCurrentCodePackage] = useState(language == 'python' ? pythonFetchedCodePackages[progress] : javascriptFetchedCodePackages[progress])
   const [isCorrect, setIsCorrect] = useState(false)
   const [isAnswered, setIsAnswered] = useState(false)
   const [isButtonDisabled, setIsButtonDisabled] = useState(false)
@@ -25,7 +30,7 @@ function Game() {
   const [randomIndex, setRandomIndex] = useState(null) // This is only for random feedback messages, not important
 
   useEffect(() => { // update and reset information when jumping to the next question
-    setCurrentCodePackage(fetchedCodePackages[progress])
+    setCurrentCodePackage(language == 'python' ? pythonFetchedCodePackages[progress] : javascriptFetchedCodePackages[progress])
     setIsAnswered(false)
     setIsCorrect(false)
     setIsButtonDisabled(false)
@@ -35,17 +40,18 @@ function Game() {
 
   const submitCode = async () => {
 
-    await axios.post('http://127.0.0.1:5000/code', {"code-package": currentCodePackage}) // send codes to backend for processing
-      .then(data => {   // data.data will contain the debugged function return value
-        console.log('data: ', data)
-        if (data.data == currentCodePackage['snippet']['return']) { // we compare the incoming value with the saved return value from the database
-          setIsCorrect(true)            // if it matches
+    await axios.post(language == 'python' ? 'http://127.0.0.1:5000/code' : 'http://localhost:3000/code', {"code-package": currentCodePackage}) // send codes to backend for processing
+      .then(data => {   // data.data will contain the debugged function return values in an array
+        console.log('data.data: ', data.data)
+        if (data.data[0] === currentCodePackage['snippet']['return-1'] &&
+            data.data[1] === currentCodePackage['snippet']['return-2'] ) { // we compare the incoming values with the saved return values from the database
+          setIsCorrect(true)            // if both match
           setRandomIndex(() => Math.floor(Math.random() * correctMessages.length))
           setIsButtonDisabled(true)
           const doneTime = new Date().getTime()
           const solvingSeconds = Math.round((doneTime - solvingTime) / 100) / 10
           setSolvingTime(solvingSeconds)
-          // Here some code to save user's time to solve this bug. Maybe update if this is the quickest?
+          // Here some code to save this duration to user's profile. Maybe update if this is the quickest?
         } else {
           setIsCorrect(false)           // if it doesn't
           setRandomIndex(() => Math.floor(Math.random() * incorrectMessages.length))
@@ -58,7 +64,8 @@ function Game() {
   const nextCode = () => {
 
     setProgress(prev => prev + 1)
-    if (progress + 1 === fetchedCodePackages.length) {
+    const currentLanguagePackage = language == 'python' ? pythonFetchedCodePackages : javascriptFetchedCodePackages
+    if (progress + 1 === currentLanguagePackage.length) {
       navigate('/completed')
     }
     // Here some code to save user's new progress in the database
@@ -74,6 +81,7 @@ function Game() {
         <FlashMessage style={{display: isCorrect ? 'flex' : 'none'}} text={`${solvingTime} s`}/>
       <CodeMirror value={currentCodePackage['snippet']['body']} 
               theme={dracula}
+              indentWithTab={true}
               onChange={(editor, change) => {
                 setCurrentCodePackage(() => {
                   return {'id': currentCodePackage['id'], // this is basically changes the 'body' value only. It's the code from user's input. We need the other original values
@@ -81,13 +89,15 @@ function Game() {
                     'description': currentCodePackage['snippet']['description'],
                     'import': currentCodePackage['snippet']['import'],
                     'body' : editor,
-                    'to-execute': currentCodePackage['snippet']['to-execute'],
-                    'return' : currentCodePackage['snippet']['return']
+                    'to-execute-1': currentCodePackage['snippet']['to-execute-1'],
+                    'return-1' : currentCodePackage['snippet']['return-1'],
+                    'to-execute-2': currentCodePackage['snippet']['to-execute-2'],
+                    'return-2' : currentCodePackage['snippet']['return-2']
                     }
                   }
                 })
               }}
-              extensions={[langs.python()]} />
+              extensions={[langs.javascript(), langs.python()]} />
       </div>
       {/* <Image image="" /> */}
       {/* <Input name="" text="Which line number is wrong?" /> */}
