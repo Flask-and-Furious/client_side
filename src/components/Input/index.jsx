84580f8967998b92b83116bceb389686c8325db9
vivian <vivian.v18723@gmail.com>
2022-09-26 16:59:02 +0100

    add & use prop for role attribute

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import styles from './input.module.css'
- 
-function Input({ name, classVariant, type, children, text}) {
- return (
- 
-   <>
-      <label htmlFor={name}>{text}</label>
-     <input role="input" id={name} name={name} type={type}/>
+import React from "react";
+import styles from "./input.module.css";
 
+function Input({ name, classVariant, type, children, text, testRole }) {
+  return (
+    <>
+      <label htmlFor={name}>{text}</label>
+      {/* <label htmlFor={name}>{children}</label> */}
+      <input role={testRole} id={name} name={name} type={type} />
     </>
- 
- )
+  );
 }
- 
-export default Input
+
+export default Input;
