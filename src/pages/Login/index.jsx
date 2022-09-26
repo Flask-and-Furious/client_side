84df8802e47e21594984f7c8273c1612fac961ad
vivian <vivian.v18723@gmail.com>
2022-09-26 16:45:34 +0100

    edit file to pass props

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 
-import { Title, Input } from "../../components"
+import { Title, Input, Button } from "../../components";
 
 function Login() {
-    return (
-        <div>
-            <Title>Insert App Name</Title>
-            <form>
-                <Input type="text" name="username">Username</Input>
-                <Input type="password" name="password">Password</Input>
-                <Input type="submit" name="login">Login</Input>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <Title title="Flask & Furious" />
+      <form>
+        <Input name="username" id="username" type="text" text="Username" />
+        <Input name="password" id="password" type="password" text="Password" />
+        <Button text="Login" />
+      </form>
+
+      {/* <Title>Insert App Name</Title> */}
+      {/* <form>
+        <Input type="text" name="username">Username</Input>
+        <Input type="password" name="password">Password</Input>
+        <Input type="submit" name="login">Login</Input>
+      </form> */}
+    </div>
+  );
 }
 
-export default Login
+export default Login;
