45f46a137c30d47bfb5b90f01dbb2cb7fd877215
vivian <vivian.v18723@gmail.com>
2022-09-26 17:11:35 +0100

    edit components to pass correct props

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,20 +1,53 @@
 import React from "react";
 
-import { Title, Input } from "../../components"
+import { Title, Input, Button } from "../../components";
 
 function Register() {
-    return (
-        <div>
-            <Title>Insert App Name</Title>
-            <form>
-                <Input type="text" name="username">Username</Input>
-                <Input type="text" name="email">Email</Input>
-                <Input type="password" name="password">Password</Input>
-                <Input type="password" name="confirm-password">Confirm Password</Input>
-                <Input type="submit" name="Register">Register</Input>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <Title title="Flask & Furious" />
+      <form>
+        <Input
+          name="username"
+          id="username"
+          type="text"
+          text="Username"
+          testRole="username"
+        />
+        <Input
+          name="email"
+          id="email"
+          type="email"
+          text="Email"
+          testRole="email"
+        />
+        <Input
+          name="password"
+          id="password"
+          type="password"
+          text="Password"
+          testRole="password"
+        />
+        <Input
+          name="password"
+          id="password"
+          type="password"
+          text="Confirm Password"
+          testRole="confirm-password"
+        />
+        <Button text="Register" testRole="register" />
+      </form>
+
+      {/* <Title>Insert App Name</Title>
+      <form>
+          <Input type="text" name="username">Username</Input>
+          <Input type="text" name="email">Email</Input>
+          <Input type="password" name="password">Password</Input>
+          <Input type="password" name="confirm-password">Confirm Password</Input>
+          <Input type="submit" name="Register">Register</Input>
+      </form> */}
+    </div>
+  );
 }
 
-export default Register
+export default Register;
