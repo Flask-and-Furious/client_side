75c2c8012587af31695d7f64c9aed5e3b14ac79f
vivian <vivian.v18723@gmail.com>
2022-09-27 12:18:07 +0100

    use props to dynamically render text add more components add navigation

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { Button, Title } from "../../components";
+import { Button, Subtitle, Title } from "../../components";
 
 function DashboardPage() {
-    return (<>
-        <div>
-            <Title>Debugging Challenge</Title>
-            <p>Current Level: EASY</p>
-        </div>
+  return (
+    <>
+      <div>
+        <Title title="Flask & Furious" />
+        <Subtitle subtitle="Your Dashboard" />
+        <p>Current Level: EASY</p>
+      </div>
 
-          
-        <div>
-          <Button/>
-        </div>
-        </>
-
-    )
+      <div>
+        <Button text="Let's Play" nextPage="/language" />
+      </div>
+    </>
+  );
 }
 
-export default DashboardPage
+export default DashboardPage;
