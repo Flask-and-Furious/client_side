7b7d035a3ee92af0006fb79f262e8d2e929997ea
vivian <vivian.v18723@gmail.com>
2022-09-27 12:49:36 +0100

    use components to create completed page

--- src/pages/Completed/index.jsx
+++ src/pages/Completed/index.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { Button, Subtitle, Title } from "../../components";
 
 function CompletedPage() {
-    return (<>
-        <div>Well Done!</div>
-        
-        <a href="/dashboard">Go to Dashboard</a>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <Title title="Flask & Furious" />
+        <Subtitle subtitle="Well Done!" />
+        <Button text="Go to Dashboard" nextPage="/dashboard" />
+      </div>
+
+      {/* <div>Well Done!</div>
+
+      <a href="/dashboard">Go to Dashboard</a> */}
+    </>
+  );
 }
 
-export default CompletedPage
+export default CompletedPage;
