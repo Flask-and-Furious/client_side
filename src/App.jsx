ba291aee540937f5e6e9ddff1ca567e09f84ede0
vivian <vivian.v18723@gmail.com>
2022-09-27 20:09:41 +0100

    restrict access to routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import * as Pages from "./pages";
+import PrivateRoutes from "./utils/PrivateRoutes";
 import "./App.css";
 
 function App() {
@@ -10,12 +11,18 @@ function App() {
       <Route path="/" element={<Pages.Home />}></Route>
       <Route path="/login" element={<Pages.LoginPage />}></Route>
       <Route path="/register" element={<Pages.RegisterPage />}></Route>
-      <Route path="/dashboard" element={<Pages.DashboardPage />}></Route>
-      <Route path="/language" element={<Pages.LanguagePage />}></Route>
-      <Route path="/game" element={<Pages.GamePage />}></Route>
-      <Route path="/completed" element={<Pages.CompletedPage />}></Route>
-      {/* <Route path="/difficulty" element={<Pages.DifficultyPage />}></Route> */}
+
+      {/* restricted access */}
+      <Route element={<PrivateRoutes />}>
+        <Route path="/dashboard" element={<Pages.DashboardPage />} />
+        <Route path="/language" element={<Pages.LanguagePage />} />
+        <Route path="/game" element={<Pages.GamePage />} />
+        <Route path="/completed" element={<Pages.CompletedPage />} />
+      </Route>
+
       <Route path="*" element={<Pages.PageNotFound />}></Route>
+
+      {/* <Route path="/difficulty" element={<Pages.DifficultyPage />}></Route> */}
     </Routes>
   );
 }
