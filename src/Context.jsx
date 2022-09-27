0d1b5160839a263e57e2ebf98d7f7c4322f84cb5
vivian <vivian.v18723@gmail.com>
2022-09-27 18:39:35 +0100

    remove unused state variable

--- src/Context.jsx
+++ src/Context.jsx
@@ -4,7 +4,6 @@ export const Context = React.createContext();
 export const ContextProvider = ({ children }) => {
   const [codeLanguage, setCodeLanguage] = useState("");
   const [storedSessionUser, setStoredSessionUser] = useState(""); // needs to come from SQL database
-  // const [storedFullUserDetails, setStoredFullUserDetails] = useState(""); // needs to come from SQL database
   const [isValidUser, setIsValidUser] = useState(true);
 
   return (
