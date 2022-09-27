d8f9c9a9cbb7077f3cb48af1946c7771dea873f1
vivian <vivian.v18723@gmail.com>
2022-09-27 11:59:05 +0100

    add useNavigate to move to dashboard after log in

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,51 +1,83 @@
 import React, { useState } from "react";
-import axios from 'axios'
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-import { Title, Input } from "../../components"
+import { Title, Input, Button } from "../../components";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const goTo = useNavigate();
 
+  // moves to dashboard after logging in
+  const handleNavigate = () => {
+    goTo("/dashboard", { replace: true });
+  };
 
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+    handleNavigate();
 
-        try {
-            const options = {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body : JSON.stringify(Object.fromEntries(new FormData(e.target)))
+    try {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Object.fromEntries(new FormData(e.target))),
+      };
+      console.log("body:", options.body);
+      const res = await fetch(
+        "https://python-debug.herokuapp.com/login",
+        options
+      );
+      const data = await res.json();
+      console.log("Res:", data);
 
-
-                
-            }
-            console.log("body:", options.body)
-            const res = await fetch('https://python-debug.herokuapp.com/login', options)
-            const data = await res.json()
-            console.log("Res:", data)
-            
-            return data
-            
-        } catch (err) {
-            console.log("Error :", err)
-        }
-
+      return data;
+    } catch (err) {
+      console.log("Error :", err);
     }
+  };
 
-    return (
-        <div>
-            <Title>Insert App Name</Title>
-            <form onSubmit={handleSubmit}>
-                <Input type="text" name="username" onChange={(e)=>{ setUsername(e.target.value)}}>Username</Input>
-                <Input type="password" name="password" onChange={(e)=>{ setPassword(e.target.value)}}>Password</Input>
-                <Input type="submit" name="login">Login</Input>
-            </form>
-        </div>
-    )
+  return (
+    <>
+      <div>
+        <Title title="Flask & Furious" />
+        <form onSubmit={handleSubmit}>
+          <Input
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            name="username"
+            id="username"
+            type="text"
+            text="Username"
+          />
+          <Input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            name="password"
+            id="password"
+            type="password"
+            text="Password"
+          />
+          <Button text="Login" />
+        </form>
+      </div>
+
+      {/* <div>
+        <Title>Insert App Name</Title>
+        <form onSubmit={handleSubmit}>
+            <Input type="text" name="username" onChange={(e)=>{ setUsername(e.target.value)}}>Username</Input>
+            <Input type="password" name="password" onChange={(e)=>{ setPassword(e.target.value)}}>Password</Input>
+            <Input type="submit" name="login">Login</Input>
+        </form>        
+      </div> */}
+    </>
+  );
 }
 
-export default Login
+export default Login;
