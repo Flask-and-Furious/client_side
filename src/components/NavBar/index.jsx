09fb67d01f5f2fab8df23e101fe90312999b7bbf
vivian <vivian.v18723@gmail.com>
2022-09-27 10:45:54 +0100

    use NavLink & Outlet from react-router-dom... ...to create nav bar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,55 +1,143 @@
 import React, { useState } from "react";
+import { NavLink, Outlet } from "react-router-dom";
+
 import "./navbar.css";
 
-function Navbar() {
-  const [active, setActive] = useState("nav__menu");
-  const [icon, setIcon] = useState("nav__toggler");
-  const navToggle = () => {
-    if (active === "nav__menu") {
-      setActive("nav__menu nav__active");
-    } else setActive("nav__menu");
-
-    // Icon Toggler
-    if (icon === "nav__toggler") {
-      setIcon("nav__toggler toggle");
-    } else setIcon("nav__toggler");
-  };
+function NavBar() {
   return (
-    <nav className="nav">
-      <a href="#" className="nav__brand">
-        DEBUGGER
-      </a>
-      <ul className={active}>
-        <li className="nav__item">
-          <a href="#" className="nav__link">
-            Home
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="#" className="nav__link">
-            About
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="#" className="nav__link">
-            Contact
-          </a>
-        </li>
-        <li className="nav__item">
-          <a href="#" className="nav__link">
-            Play
-          </a>
-    
-       
-        </li>
-      </ul>
-      <div onClick={navToggle} className={icon}>
-        <div className="line1"></div>
-        <div className="line2"></div>
-        <div className="line3"></div>
-      </div>
+    <nav>
+      <NavLink
+        to="/"
+        style={({ isActive }) =>
+          isActive
+            ? { textDecoration: "underline" }
+            : { textDecoration: "none" }
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/login"
+        style={({ isActive }) =>
+          isActive
+            ? { textDecoration: "underline" }
+            : { textDecoration: "none" }
+        }
+      >
+        Login
+      </NavLink>
+      <NavLink
+        to="/register"
+        style={({ isActive }) =>
+          isActive
+            ? { textDecoration: "underline" }
+            : { textDecoration: "none" }
+        }
+      >
+        Register
+      </NavLink>
+      <NavLink
+        to="/language"
+        style={({ isActive }) =>
+          isActive
+            ? { textDecoration: "underline" }
+            : { textDecoration: "none" }
+        }
+      >
+        Language
+      </NavLink>
+      <NavLink
+        to="/difficulty"
+        style={({ isActive }) =>
+          isActive
+            ? { textDecoration: "underline" }
+            : { textDecoration: "none" }
+        }
+      >
+        Difficulty
+      </NavLink>
+      <NavLink
+        to="/game"
+        style={({ isActive }) =>
+          isActive
+            ? { textDecoration: "underline" }
+            : { textDecoration: "none" }
+        }
+      >
+        Game
+      </NavLink>
+      <NavLink
+        to="/completed"
+        style={({ isActive }) =>
+          isActive
+            ? { textDecoration: "underline" }
+            : { textDecoration: "none" }
+        }
+      >
+        Completed
+      </NavLink>
+      <NavLink
+        to="/dashboard"
+        style={({ isActive }) =>
+          isActive
+            ? { textDecoration: "underline" }
+            : { textDecoration: "none" }
+        }
+      >
+        Dashboard
+      </NavLink>
+      <Outlet />
     </nav>
   );
 }
+export default NavBar;
 
-export default Navbar;
+// function Navbar() {
+//   const [active, setActive] = useState("nav__menu");
+//   const [icon, setIcon] = useState("nav__toggler");
+//   const navToggle = () => {
+//     if (active === "nav__menu") {
+//       setActive("nav__menu nav__active");
+//     } else setActive("nav__menu");
+
+//     // Icon Toggler
+//     if (icon === "nav__toggler") {
+//       setIcon("nav__toggler toggle");
+//     } else setIcon("nav__toggler");
+//   };
+//   return (
+//     <nav className="nav">
+//       <a href="#" className="nav__brand">
+//         DEBUGGER
+//       </a>
+//       <ul className={active}>
+//         <li className="nav__item">
+//           <a href="#" className="nav__link">
+//             Home
+//           </a>
+//         </li>
+//         <li className="nav__item">
+//           <a href="#" className="nav__link">
+//             About
+//           </a>
+//         </li>
+//         <li className="nav__item">
+//           <a href="#" className="nav__link">
+//             Contact
+//           </a>
+//         </li>
+//         <li className="nav__item">
+//           <a href="#" className="nav__link">
+//             Play
+//           </a>
+
+//         </li>
+//       </ul>
+//       <div onClick={navToggle} className={icon}>
+//         <div className="line1"></div>
+//         <div className="line2"></div>
+//         <div className="line3"></div>
+//       </div>
+//     </nav>
+//   );
+// }
