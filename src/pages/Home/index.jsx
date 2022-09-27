5a417c1d12cd687df7ac2b1ba5fa75c944512247
vivian <vivian.v18723@gmail.com>
2022-09-27 11:12:39 +0100

    pass props for button components... ...to move to next specified page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,8 +8,8 @@ function Home() {
       <Title title="Flask & Furious" />
       <Subtitle subtitle="Catchy slogan goes here" />
       <Image image="" altVal="hero image goes here" />
-      <Button text="Login" />
-      <Button text="Register" />
+      <Button text="Login" nextPage="/login" />
+      <Button text="Register" nextPage="/register" />
       <Subtitle subtitle="Article 1 name goes here" />
       <Image image="" altVal="article 1 image goes here" />
       <Subtitle subtitle="Article 2 name goes here" />
