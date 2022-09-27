1e1bad1e0329c2d3d55fafb8c103f7d978ee9fb9
vivian <vivian.v18723@gmail.com>
2022-09-27 21:43:38 +0100

    remove storedSessionUser state variable usage

--- src/pages/Completed/index.jsx
+++ src/pages/Completed/index.jsx
@@ -1,16 +1,11 @@
-import React, { useContext } from "react";
-
 import { Button, Subtitle, Title } from "../../components";
-import { Context } from "../../Context";
 
 function CompletedPage() {
-  const { storedSessionUser, setStoredSessionUser } = useContext(Context);
-
   return (
     <>
       <div>
         <Title title="Flask & Furious" />
-        <Subtitle subtitle={`Well Done ${storedSessionUser}!`} />
+        <Subtitle subtitle={"Well Done!"} />
         <Button text="Go to Dashboard" nextPage="/dashboard" />
       </div>
 
