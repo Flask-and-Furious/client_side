0b1b7a0975f8b0e2023027fe34e074ad7699e427
vivian <vivian.v18723@gmail.com>
2022-09-27 13:57:05 +0100

    add new state variable for Context.Provider

--- src/Context.jsx
+++ src/Context.jsx
@@ -3,9 +3,10 @@ import React, { useState } from "react";
 export const Context = React.createContext();
 export const ContextProvider = ({ children }) => {
   const [codeLanguage, setCodeLanguage] = useState("");
+  const [storedUsername, setStoredUsername] = useState("");
 
   return (
-    <Context.Provider value={{ codeLanguage, setCodeLanguage }}>
+    <Context.Provider value={{ codeLanguage, setCodeLanguage, storedUsername, setStoredUsername }}>
       {children}
     </Context.Provider>
   );
