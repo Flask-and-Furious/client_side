59f6be7c7a678d1a7997c9044119b21d87e05377
vivian <vivian.v18723@gmail.com>
2022-09-27 14:01:07 +0100

    use useContext to set current user... ...as the entered user in the input

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,24 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
 import { Title, Input, Button, Subtitle } from "../../components";
+import { Context } from "../../Context";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const { storedUsername, setStoredUsername } = useContext(Context);
   const goTo = useNavigate();
 
   // moves to dashboard after logging in
   const handleNavigate = () => {
-    goTo("/dashboard", { replace: true });
+    goTo("/dashboard");
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     handleNavigate();
+
+    setStoredUsername(username)
 
     try {
       const options = {
