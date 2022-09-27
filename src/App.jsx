27478443fc12b32f461013cdfdf19ac2d338fb56
vivian <vivian.v18723@gmail.com>
2022-09-27 12:31:00 +0100

    rearrange route order

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,11 @@ function App() {
       <Route path="/" element={<Pages.Home />}></Route>
       <Route path="/login" element={<Pages.LoginPage />}></Route>
       <Route path="/register" element={<Pages.RegisterPage />}></Route>
+      <Route path="/dashboard" element={<Pages.DashboardPage />}></Route>
       <Route path="/language" element={<Pages.LanguagePage />}></Route>
-      <Route path="/difficulty" element={<Pages.DifficultyPage />}></Route>
       <Route path="/game" element={<Pages.GamePage />}></Route>
       <Route path="/completed" element={<Pages.CompletedPage />}></Route>
-      <Route path="/dashboard" element={<Pages.DashboardPage />}></Route>
+      <Route path="/difficulty" element={<Pages.DifficultyPage />}></Route>
       <Route path="*" element={<Pages.PageNotFound />}></Route>
     </Routes>
   );
