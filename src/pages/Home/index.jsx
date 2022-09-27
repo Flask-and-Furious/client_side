43233edaf344259a0e2b789b7989ef5b9ca50a7a
vivian <vivian.v18723@gmail.com>
2022-09-27 13:59:35 +0100

    use useContext to render login & register btns... ...based on if there is a current user

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 
 import { Button, Image, Subtitle, Title } from "../../components";
+import { Context } from "../../Context";
 
 function Home() {
+  const { storedUsername, setStoredUsername } = useContext(Context);
+
   return (
     <>
       <Title title="Flask & Furious" />
       <Subtitle subtitle="Catchy slogan goes here" />
       <Image image="" altVal="hero image goes here" />
-      <Button text="Login" nextPage="/login" />
-      <Button text="Register" nextPage="/register" />
+      {storedUsername ? null : (
+        <>
+          <Button text="Login" nextPage="/login" />
+          <Button text="Register" nextPage="/register" />
+        </>
+      )}
       <Subtitle subtitle="Article 1 name goes here" />
       <Image image="" altVal="article 1 image goes here" />
       <Subtitle subtitle="Article 2 name goes here" />
