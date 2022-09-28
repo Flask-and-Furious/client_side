538057d0b45ecdc0954a1ac47b0709f069fae717
johnpascual <ashleypascual@hotmail.es>
2022-09-28 11:39:33 +0100

    langauge page edit

--- src/pages/Language/index.jsx
+++ src/pages/Language/index.jsx
@@ -53,10 +53,10 @@ function LanguagePage() {
       </div>
       <div className="language-example">
         {codeLanguage =="python" ? <><p>Python example</p>
-          <p>Python example question.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </>
         : codeLanguage == "javascript" ? <><p>Javascript Example</p>
-          <p>Javascript example question.</p></>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></>
         : <><p>Example</p><p>Choose a language to see an example question</p></>  
         }
       </div>
