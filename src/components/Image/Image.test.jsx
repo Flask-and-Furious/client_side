34f048a5f55e47813eae6958c679826108a8d420
vivian <vivian.v18723@gmail.com>
2022-09-28 16:00:22 +0100

    add passing test

--- src/components/Image/Image.test.jsx
+++ src/components/Image/Image.test.jsx
@@ -1,13 +1,27 @@
-/** @jest-environment jsdom */
-import React from "react";
-import { render } from "@testing-library/react";
+/**
+ * @jest-environment jsdom
+ */
 
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import React from "react";
+import userEvent from "@testing-library/user-event";
+import { BrowserRouter } from "react-router-dom";
 import Image from ".";
 
-describe("image component ", () => {
-  it("renders image ", () => {
+describe("Image component", () => {
+  beforeEach(() => {
     const imgLabel = "";
 
-    render(<Image label={imgLabel} />);
+    render(
+      <BrowserRouter>
+        <Image label={imgLabel} />
+      </BrowserRouter>
+    );
+  });
+
+  it("Renders an image", () => {
+    const img = screen.queryByRole("img");
+    expect(img).toBeInTheDocument();
   });
 });
