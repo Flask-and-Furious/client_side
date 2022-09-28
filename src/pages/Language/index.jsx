6e7dacc319fa429936f37f56e91f24ad4c353405
“crlnvls” <cveloso@hotmail.co.uk>
2022-09-29 00:55:35 +0100

    change p for h2

--- src/pages/Language/index.jsx
+++ src/pages/Language/index.jsx
@@ -47,10 +47,10 @@ function LanguagePage() {
         ) : null}
       </div>
       <div className="language-example">
-        {codeLanguage =="python" ? <><p>Python example</p>
+        {codeLanguage =="python" ? <><h2>Python example</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </>
-        : codeLanguage == "javascript" ? <><p>Javascript Example</p>
+        : codeLanguage == "javascript" ? <><h2>Javascript Example</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></>
         : <><p>Example</p><p>Choose a language to see an example question</p></>  
         }
