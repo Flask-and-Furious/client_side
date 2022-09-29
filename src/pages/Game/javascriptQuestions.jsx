b363cb04832f12bbe67b60ecc822f03c92f34dbf
azotamiota <azotamiota@gmail.com>
2022-09-29 12:33:42 +0100

    update questions content

--- src/pages/Game/javascriptQuestions.jsx
+++ src/pages/Game/javascriptQuestions.jsx
@@ -28,59 +28,5 @@ export const javascriptFetchedCodePackages = [
           [ 6, 12, 18, 24, 30, 36 ]
         ]
       }
-    },
-    {'id': 2,                       
-     'snippet': {
-        'description': 'This function should create NxN multiplication table, of size provided in parameter.\nFor example, when given size is 4, the returned value should look like this:\n[[ 1, 2, 3, 4 ],\n[ 2, 4, 6, 8 ],\n[ 3, 6, 9, 12 ],\n[ 4, 8, 12, 16 ]]',
-        'import': 'multiplicationTable',
-        'body' : 'const multiplicationTable = function (size) {\n  let outerArr = [[...Array(size).keys()].map(x => x + 1)]\n  let multiplier = 2;\n  while (outerArr.length < size) {\n    let innerArr = outerArr.map(num => num * multiplier);\n    outerArr.push(innerArr);\n    multiplier++;\n  }\n\n  return outerArr;\n};',
-        'to-execute-1': 'multiplicationTable(3)',
-        'return-1' : [ [ 1, 2, 3 ], [ 2, 4, 6 ], [ 3, 6, 9 ] ],
-        'to-execute-2': 'multiplicationTable(6)',
-        'return-2' : [
-          [ 1, 2, 3, 4, 5, 6 ],
-          [ 2, 4, 6, 8, 10, 12 ],
-          [ 3, 6, 9, 12, 15, 18 ],
-          [ 4, 8, 12, 16, 20, 24 ],
-          [ 5, 10, 15, 20, 25, 30 ],
-          [ 6, 12, 18, 24, 30, 36 ]
-        ]
-      }
-    },
-    {'id': 3,                       
-     'snippet': {
-        'description': 'This function should create NxN multiplication table, of size provided in parameter.\nFor example, when given size is 4, the returned value should look like this:\n[[ 1, 2, 3, 4 ],\n[ 2, 4, 6, 8 ],\n[ 3, 6, 9, 12 ],\n[ 4, 8, 12, 16 ]]',
-        'import': 'multiplicationTable',
-        'body' : 'const multiplicationTable = function (size) {\n  let outerArr = [[...Array(size).keys()].map(x => x + 1)]\n  let multiplier = 2;\n  while (outerArr.length < size) {\n    let innerArr = outerArr.map(num => num * multiplier);\n    outerArr.push(innerArr);\n    multiplier++;\n  }\n\n  return outerArr;\n};',
-        'to-execute-1': 'multiplicationTable(3)',
-        'return-1' : [ [ 1, 2, 3 ], [ 2, 4, 6 ], [ 3, 6, 9 ] ],
-        'to-execute-2': 'multiplicationTable(6)',
-        'return-2' : [
-          [ 1, 2, 3, 4, 5, 6 ],
-          [ 2, 4, 6, 8, 10, 12 ],
-          [ 3, 6, 9, 12, 15, 18 ],
-          [ 4, 8, 12, 16, 20, 24 ],
-          [ 5, 10, 15, 20, 25, 30 ],
-          [ 6, 12, 18, 24, 30, 36 ]
-        ]
-      }
-    },
-    {'id': 4,                       
-     'snippet': {
-        'description': 'This function should create NxN multiplication table, of size provided in parameter.\nFor example, when given size is 4, the returned value should look like this:\n[[ 1, 2, 3, 4 ],\n[ 2, 4, 6, 8 ],\n[ 3, 6, 9, 12 ],\n[ 4, 8, 12, 16 ]]',
-        'import': 'multiplicationTable',
-        'body' : 'const multiplicationTable = function (size) {\n  let outerArr = [[...Array(size).keys()].map(x => x + 1)]\n  let multiplier = 2;\n  while (outerArr.length < size) {\n    let innerArr = outerArr.map(num => num * multiplier);\n    outerArr.push(innerArr);\n    multiplier++;\n  }\n\n  return outerArr;\n};',
-        'to-execute-1': 'multiplicationTable(3)',
-        'return-1' : [ [ 1, 2, 3 ], [ 2, 4, 6 ], [ 3, 6, 9 ] ],
-        'to-execute-2': 'multiplicationTable(6)',
-        'return-2' : [
-          [ 1, 2, 3, 4, 5, 6 ],
-          [ 2, 4, 6, 8, 10, 12 ],
-          [ 3, 6, 9, 12, 15, 18 ],
-          [ 4, 8, 12, 16, 20, 24 ],
-          [ 5, 10, 15, 20, 25, 30 ],
-          [ 6, 12, 18, 24, 30, 36 ]
-        ]
-      }
     }
 ]
--- src/pages/Game/pythonQuestions.jsx
+++ src/pages/Game/pythonQuestions.jsx
@@ -1,7 +1,7 @@
   export const pythonFetchedCodePackages = [
     {'id': 0,                       //  sample buggy functions
      'snippet': {
-        'description': 'Fix this simple function',
+        'description': 'Fix this simple function!',
         'import': 'add',
         'body' : 'def add(a, b):\n    return a + a\n\n# Expectation: add(3,2) => 5',
         'to-execute-1': 'add(3,2)',
@@ -10,55 +10,31 @@
         'return-2' : 5555
       }
     },
-    {'id': 1,                       //  sample buggy functions
+    {'id': 1,                       
      'snippet': {
-        'description': 'Make the input text upper case',
-        'import': 'upper_case',
-        'body' : 'def make_text_upper_case(string):\n    upper_case = string.upper()\n\nreturn upper_case\n\n# Expectation:\n# make_text_upper_case("hire us!")\n# => HIRE US!',
-        'to-execute-1': 'upper_case("qwerty")',
+        'description': 'Make the input text upper case!',
+        'import': 'make_text_upper_case',
+        'body' : 'def make_text_upper_case(string):\n    upper_case += string.upper()\n\n    return upper_case\n\n# Expectation:\n# make_text_upper_case("hire us!")\n# => HIRE US!',
+        'to-execute-1': 'make_text_upper_case("qwerty")',
         'return-1' : "QWERTY",
-        'to-execute-2': 'upper_case("Game over!")',
+        'to-execute-2': 'make_text_upper_case("Game over!")',
         'return-2' : "GAME OVER!"
       }
     },
-    {'id': 1,
+    {'id': 2,                      
      'snippet': {
-        'description': 'Remove spaces from the input string',
-        'import': 'no_space',
-        'body' : 'def no_space(string):\n    for i in string:\n        if i == " ":\n            i.replace(" ","")\n\n    return string\n\n# Expectation:\n# no_space("H i  r eU s  ! ")\n# => HireUs!',
-        'to-execute-1': 'no_space("    he     l l o! ")',
-        'return-1' : 'hello!',
-        'to-execute-2': 'no_space("q  w  e     r t yyyy  ")',
-        'return-2' : 'qwertyyyy'
-      }
-    },
-    {'id': 2,
-     'snippet': {
-        'description': 'Take a look at the series in the comments. Debug this function which returns a string of the sum rounded to two decimals of the series upto nth term.',
-        'import': 'show',
-        'body' : "def series_sum(n):\n    if n == 0:\n        return '0.00'\n    sum = 0\n    for i in range(1, n):\n        sum += 1 / i\n    sum = str(round(sum, 2))\n    return sum\n\n# 1 => '1.00' (1)\
-                  \n# 2 => '1.5' (1 + 1/2)\n# 5 => '2.28' (1 + 1/2 + 1/3 + 1/4 + 1/5)\n# If the given value is 0,\n# it should return '0.00'",
-        'to-execute-1': 'series_sum(5)',
-        'return-1' : '2.28',
-        'to-execute-2': 'series_sum(0)',
-        'return-2' : '0.00'
+        'description': 'Compare two numbers and return "true" if the first one is greater than or equal to the second one. Otherwise return "false". Input values will be integers.',
+        'import': 'compare',
+        'body' : 'def compare(num1, num2):\n    if num1 > num2:\n        return "true"\n    else:\n        return "false"\n\n# Expectation:\n# compare(6, 2) => "true"\n# compare(-4, 9) => "false"\n# compare(5, 5) => "true"',
+        'to-execute-1': 'compare(3,26)',
+        'return-1' : 'false',
+        'to-execute-2': 'compare(66,66)',
+        'return-2' : 'true'
       }
     },
     {'id': 3,
      'snippet': {
-        'description': 'Take a look at the series in the comments. Debug this function which returns a string of the sum rounded to two decimals of the series upto nth term.',
-        'import': 'show',
-        'body' : "def series_sum(n):\n    if n == 0:\n        return '0.00'\n    sum = 0\n    for i in range(1, n):\n        sum += 1 / i\n    sum = str(round(sum, 2))\n    return sum\n\n# 1 => '1.00' (1)\
-                  \n# 2 => '1.5' (1 + 1/2)\n# 5 => '2.28' (1 + 1/2 + 1/3 + 1/4 + 1/5)\n# If the given value is 0,\n# it should return '0.00'",
-        'to-execute-1': 'series_sum(5)',
-        'return-1' : '2.28',
-        'to-execute-2': 'series_sum(0)',
-        'return-2' : '0.00'
-      }
-    },
-    {'id': 4,
-     'snippet': {
-        'description': 'Take a look at the series in the comments. Debug this function which returns a string of the sum rounded to two decimals of the series upto nth term.',
+        'description': 'Take a look at the series in the comments. Debug this function which returns a string of the sum. The result should be rounded to two decimals of the series upto nth term.',
         'import': 'show',
         'body' : "def series_sum(n):\n    if n == 0:\n        return '0.00'\n    sum = 0\n    for i in range(1, n):\n        sum += 1 / i\n    sum = str(round(sum, 2))\n    return sum\n\n# 1 => '1.00' (1)\
                   \n# 2 => '1.5' (1 + 1/2)\n# 5 => '2.28' (1 + 1/2 + 1/3 + 1/4 + 1/5)\n# If the given value is 0,\n# it should return '0.00'",
